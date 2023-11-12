5bc0fab533402ad7e5bb12e05ff0673eaff3f89e
Luis Felipe Risch <luis.risch@gmail.com>
2023-11-12 13:13:31 -0300

    feat: adjust considering new lib

--- main.c
+++ main.c
@@ -1,77 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "memalloc.h"
 
-#define BUSY 1
-#define FREE 0
-
-void *init_heap;
-void *top_heap;
-
-void setup_brk(){
-  init_heap = sbrk(0);
-  top_heap  = sbrk(0);
-}
-
-void dismiss_brk(){
-  brk(init_heap);
-}
-
-void *find_free_block(unsigned long int bytes){
-  void *free_block    = NULL;
-  void *current_block = init_heap;
-
-  while (current_block < top_heap && free_block == NULL)
-  {
-    unsigned long int *state       = (unsigned long int *) current_block;
-    unsigned long int *bytes_qnt   = (unsigned long int *) (current_block + 8);
-
-    if(*state == FREE && *bytes_qnt >= bytes){
-      *state = BUSY;
-      free_block = current_block;
-    }
-
-    current_block += (16 + *bytes_qnt);
-  }
-
-  return free_block;
-}
-
-void *create_new_block(unsigned long int bytes){
-  unsigned long int displacement = 16 + bytes;
-  unsigned long int *state       = (unsigned long int *) top_heap;
-  unsigned long int *bytes_qnt   = (unsigned long int *) (top_heap + 8);
-  void *new_block                = top_heap;
-
-  *state = BUSY;
-  *bytes_qnt = bytes;
-
-  top_heap += displacement;
-
-  return new_block;
-}
-
-void *memory_alloc(unsigned long int bytes){
-  void *new_block, *pointer, *free_block;
-
-  free_block = find_free_block(bytes);
-
-  if(free_block != NULL){
-    pointer = free_block + 16;
-  } else {
-    new_block = create_new_block(bytes);
-
-    pointer = new_block + 16;
-  }
-
-  return pointer;
-}
-
-void memory_free(void *pointer){
-  unsigned long int *state = (unsigned long int *) (pointer - 16);
-
-  *state = FREE;
-}
+extern void *init_heap;
 
 int main(){
   setup_brk();
@@ -88,11 +17,21 @@ int main(){
 
   printf("state %ld\n", *((unsigned long int *)(pointer - 16)));
 
-  void *pointer_two = memory_alloc(100);
+  void *pointer_two = memory_alloc(83);
 
   printf("&pointer_two: %p\n", pointer_two);
-
   printf("state %ld\n", *((unsigned long int *)(pointer_two - 16)));
+  printf("qnt %ld\n", *((unsigned long int *)(pointer_two - 8)));
+
+  printf("&pointer new: %p\n", pointer_two + 83 + 16);
+  printf("state new %ld\n", *((unsigned long int *)(pointer_two + 83)));
+  printf("qnt new %ld\n", *((unsigned long int *)(pointer_two  + 83 + 8)));
+
+  void *pointer_three = memory_alloc(1);
+
+  printf("&pointer_two: %p\n", pointer_three);
+  printf("state %ld\n", *((unsigned long int *)(pointer_three - 16)));
+  printf("qnt %ld\n", *((unsigned long int *)(pointer_three - 8)));
 
   printf("init heap: %p\n", init_heap);
 
