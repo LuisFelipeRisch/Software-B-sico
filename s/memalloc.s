2d062b51dbfe941bab8ed7383ffc87ddabbbdc93
Luis Felipe Risch <luis.risch@gmail.com>
2023-11-18 16:00:59 -0300

    finished to comment

--- s/memalloc.s
+++ s/memalloc.s
@@ -84,73 +84,74 @@ end_while:
 	popq	%rbp
 	ret
 
+# Creates a new block. Returns the initial position of the created block
+# void *create_new_block(unsigned long int bytes)
 create_new_block:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	movq	%rdi, -40(%rbp)
-	movq	-40(%rbp), %rax
-	addq	$16, %rax
-	movq	%rax, -32(%rbp)
-	movq	top_heap(%rip), %rax
-	movq	%rax, -24(%rbp)
-	movq	top_heap(%rip), %rax
-	addq	$8, %rax
-	movq	%rax, -16(%rbp)
-	movq	top_heap(%rip), %rax
-	movq	%rax, -8(%rbp)
-	movq	-24(%rbp), %rax
-	movq	$1, (%rax)
-	movq	-16(%rbp), %rax
-	movq	-40(%rbp), %rdx
-	movq	%rdx, (%rax)
-	movq	top_heap(%rip), %rdx
-	movq	-32(%rbp), %rax
-	addq	%rdx, %rax
-	movq	%rax, top_heap(%rip)
-	movq	-8(%rbp), %rax
+	subq	$32, %rsp							# making 32 bytes space in the stack
+	movq	%rdi, -32(%rbp)				# (%rbp  - 32) = %rdi -> bytes
+	movq	-32(%rbp), %rax				# %rax = (%rbp  - 32)
+	addq	$16, %rax							# %rax += 16 -> displacement
+	movq	%rax, -24(%rbp)				# (%rbp  - 24) = %rax
+	movq	top_heap(%rip), %rax	# %rax = top_heap
+	movq	%rax, -16(%rbp)				# (%rbp  - 16) = top_heap
+	addq	$8, %rax							# %rax += 8	-> block.bytes_qnt
+	movq	%rax, -8(%rbp)				# (%rbp  - 8) = block.bytes_qnt
+	movq	top_heap(%rip), %rax	# %rax = top_heap
+	movq	$1, (%rax)						# setting created block to busy
+	movq	-8(%rbp), %rax				# %rax = block.bytes_qnt
+	movq	-32(%rbp), %rdx				# %rdx = bytes
+	movq	%rdx, (%rax)					# setting the value indicated by the memory location of %rax with %rdx
+	movq	top_heap(%rip), %rdx	# %rdx = top_heap
+	movq	-24(%rbp), %rax				# %rax = displacement
+	addq	%rdx, %rax						# %rax += %rdx
+	movq	%rax, top_heap(%rip)	# top_heap = %rax
+	movq	-16(%rbp), %rax				# getting the initial position of the created block
+	addq $32, %rsp							# freeing up stack 32 bytes space
 	popq	%rbp
 	ret
 
+# If the free block have enough data space to create new block, it will create.
+# void try_to_alloc_new_block(void *block, unsigned long int bytes)
 try_to_alloc_new_block:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	movq	%rdi, -40(%rbp)
-	movq	%rsi, -48(%rbp)
-	movq	-40(%rbp), %rax
-	addq	$8, %rax
-	movq	%rax, -32(%rbp)
-	movq	-32(%rbp), %rax
-	movq	(%rax), %rax
-	subq	-48(%rbp), %rax
-	cmpq	$16, %rax
-	jbe	.L14
-	movq	-40(%rbp), %rax
-	addq	$16, %rax
-	movq	%rax, -24(%rbp)
-	movq	-24(%rbp), %rdx
-	movq	-48(%rbp), %rax
-	addq	%rdx, %rax
-	movq	%rax, -16(%rbp)
-	movq	-48(%rbp), %rax
-	leaq	8(%rax), %rdx
-	movq	-24(%rbp), %rax
-	addq	%rdx, %rax
-	movq	%rax, -8(%rbp)
-	movq	-16(%rbp), %rax
-	movq	$0, (%rax)
-	movq	-32(%rbp), %rax
-	movq	(%rax), %rax
-	subq	-48(%rbp), %rax
-	leaq	-16(%rax), %rdx
-	movq	-8(%rbp), %rax
-	movq	%rdx, (%rax)
-	movq	-32(%rbp), %rax
-	movq	-48(%rbp), %rdx
-	movq	%rdx, (%rax)
-	jmp	.L11
-.L14:
-	nop
-.L11:
+	subq	$48, %rsp									# making 48 bytes space in the stack
+	movq	%rdi, -40(%rbp)						# (%rbp - 40) = %rdi
+	movq	%rsi, -48(%rbp) 					# (%rbp - 48) = %rsi
+	movq	-40(%rbp), %rax						# %rax = (%rbp - 40)
+	addq	$8, %rax									# %rax += 8
+	movq	%rax, -32(%rbp) 					# (%rbp - 32) = %rax
+	movq	(%rax), %rax							# setting the value indicated by the memory location of %rax in %rax
+	subq	-48(%rbp), %rax 					# %rax -= (%rbp - 48)
+	cmpq	$16, %rax									# 16 - %rax
+	jbe	try_to_alloc_new_block_end	# Jump to L11 if %rax is below or equal to 16
+	movq	-40(%rbp), %rax						# %rax = (%rbp - 40)
+	addq	$16, %rax									# %rax += 16
+	movq	%rax, -24(%rbp)						# (%rbp - 24) = %rax
+	movq	-24(%rbp), %rdx						# %rdx = (%rbp - 24)
+	movq	-48(%rbp), %rax						# %rax = (%rbp - 48)
+	addq	%rdx, %rax								# %rax += %rdx
+	movq	%rax, -16(%rbp)						# (%rbp - 16) = %rax
+	movq	-48(%rbp), %rax						# %rax = (%rbp - 48)
+	leaq	8(%rax), %rdx							# %rdx = (%rax + 8)
+	movq	-24(%rbp), %rax						# %rax = (%rbp - 24)
+	addq	%rdx, %rax								# %rax += %rdx
+	movq	%rax, -8(%rbp)						# (%rbp - 8) = %rax
+	movq	-16(%rbp), %rax						# (%rbp - 16) = %rax
+	movq	$0, (%rax)								# %rax = 0 -> setting block to free
+	movq	-32(%rbp), %rax						# %rax = (%rbp - 32)
+	movq	(%rax), %rax							# setting the value indicated by the memory location of %rax in %rax
+	subq	-48(%rbp), %rax						# %rax -= (%rbp - 48)
+	leaq	-16(%rax), %rdx						# %rdx = (%rax - 16)
+	movq	-8(%rbp), %rax						# %rax = (%rbp - 8)
+	movq	%rdx, (%rax)							# setting the value indicated by the memory location of %rax with %rdx
+	movq	-32(%rbp), %rax						# %rax = (%rbp - 32)
+	movq	-48(%rbp), %rdx						# %rdx = (%rbp - 48)
+	movq	%rdx, (%rax)							# setting the value indicated by the memory location of %rax with %rdx
+try_to_alloc_new_block_end:
+	addq	$48, %rsp									# freeing up stack 32 bytes space
 	popq	%rbp
 	ret
 
@@ -159,25 +160,25 @@ try_to_alloc_new_block:
 memory_alloc:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	subq	$40, %rsp								# making 40 bytes space in the stack
-	movq	%rdi, -40(%rbp)					# (%rbp - 40) = bytes
+	subq	$16, %rsp								# making 16 bytes space in the stack
+	movq	%rdi, -16(%rbp)					# (%rbp - 16) = bytes
 	call	find_free_block					# find_free_block(bytes)
-	movq	%rax, -16(%rbp)					# (%rbp - 16) = %rax
-	cmpq	$0, -16(%rbp)						# (%rbp - 16) == 0 -> did not find a free block
+	movq	%rax, -8(%rbp)					# (%rbp - 8) = %rax
+	cmpq	$0, -8(%rbp)						# (%rbp - 8) == 0 -> did not find a free block
 	je	no_free_block_found				# creates a new block
 	nop														# free block found
-	movq	-40(%rbp), %rsi					# %rsi = (%rbp - 40) -> orginal value of rdi(bytes)
-	movq	-16(%rbp), %rdi					# %rdi = (%rbp - 16) -> mem addres of free block
+	movq	-16(%rbp), %rsi					# %rsi = (%rbp - 16) -> orginal value of rdi(bytes)
+	movq	-8(%rbp), %rdi					# %rdi = (%rbp - 8) -> mem addres of free block
 	call	try_to_alloc_new_block	# try_to_alloc_new_block(free_block, bytes)
-	movq	-16(%rbp), %rax					# %rax = (%rbp - 16)
+	movq	-8(%rbp), %rax					# %rax = (%rbp - 8)
 	addq	$16, %rax								# %rax += 16 -> pointing to real data address
 	jmp	mem_alloc_end
 no_free_block_found:
-	movq	-40(%rbp), %rdi					# %rdi = (%rbp - 40) -> orginal value of rdi(bytes)
+	movq	-16(%rbp), %rdi					# %rdi = (%rbp - 16) -> orginal value of rdi(bytes)
 	call	create_new_block				# create_new_block(bytes)
 	addq	$16, %rax
 mem_alloc_end:
-	addq $40, %rsp								# freeing up stack 40 bytes space
+	addq $16, %rsp								# freeing up stack 16 bytes space
 	movq %rbp, %rsp
 	popq %rbp
 	ret
