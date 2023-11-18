996e7c051be0238e2274a0505a126670799607b0
Luis Felipe Risch <luis.risch@gmail.com>
2023-11-18 11:48:29 -0300

    feat: changes made in memmory_alloc, setup_brk and dismiss_brk

--- s/memalloc.s
+++ s/memalloc.s
@@ -11,28 +11,30 @@ top_heap:
 .equ BRK_SERVICE, 12
 
 .section .text
-.globl setup_brk
-.globl dismiss_brk
+.globl 	setup_brk
+.globl 	dismiss_brk
 .globl	memory_alloc
 .globl	memory_free
 
+# call brk service and store the current value of brk into
+# memory location specified by (init_heap + %rip) and (top_heap + %rip)
 setup_brk:
 	pushq	%rbp
 	movq	%rsp, %rbp
-  movq $BRK_SERVICE, %rax
-  syscall
-	movq	%rax, init_heap(%rip)
-	movq	%rax, top_heap(%rip)
+  movq $BRK_SERVICE, %rax			# set service to brk
+  syscall											# call os
+	movq	%rax, init_heap(%rip)	# (init_heap + %rip) = brk(0)
+	movq	%rax, top_heap(%rip) 	# (top_heap + %rip) = brk(0)
 	popq	%rbp
 	ret
 
+# restores the brk to its original value
 dismiss_brk:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	movq	init_heap(%rip), %rax
-	movq	%rax, %rdi
-	movq $BRK_SERVICE, %rax
-  syscall
+	movq	init_heap(%rip), %rdi # rdi = (init_heap + %rip)
+	movq $BRK_SERVICE, %rax			# set service to brk
+  syscall											# call os
 	popq	%rbp
 	ret
 
@@ -148,36 +150,30 @@ try_to_alloc_new_block:
 	popq	%rbp
 	ret
 
+# Allocates n bytes of memory in heap and returns the memory address
+# void *memory_alloc(long int bytes);
 memory_alloc:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	subq	$40, %rsp
-	movq	%rdi, -40(%rbp)
-	movq	-40(%rbp), %rax
-	movq	%rax, %rdi
-	call	find_free_block
-	movq	%rax, -16(%rbp)
-	cmpq	$0, -16(%rbp)
-	je	.L16
-	movq	-40(%rbp), %rdx
-	movq	-16(%rbp), %rax
-	movq	%rdx, %rsi
-	movq	%rax, %rdi
-	call	try_to_alloc_new_block
-	movq	-16(%rbp), %rax
+	subq	$40, %rsp								# making 40 bytes space in the stack
+	movq	%rdi, -40(%rbp)					# (%rbp - 40) = bytes
+	call	find_free_block					# find_free_block(bytes)
+	movq	%rax, -16(%rbp)					# (%rbp - 16) = %rax
+	cmpq	$0, -16(%rbp)						# (%rbp - 16) == 0 -> did not find a free block
+	je	no_free_block_found				# creates a new block
+	nop														# free block found
+	movq	-40(%rbp), %rsi					# %rsi = (%rbp - 40) -> orginal value of rdi(bytes)
+	movq	-16(%rbp), %rdi					# %rdi = (%rbp - 16) -> mem addres of free block
+	call	try_to_alloc_new_block	# try_to_alloc_new_block(free_block, bytes)
+	movq	-16(%rbp), %rax					# %rax = (%rbp - 16)
+	addq	$16, %rax								# %rax += 16 -> pointing to real data address
+	jmp	mem_alloc_end
+no_free_block_found:
+	movq	-40(%rbp), %rdi					# %rdi = (%rbp - 40) -> orginal value of rdi(bytes)
+	call	create_new_block				# create_new_block(bytes)
 	addq	$16, %rax
-	movq	%rax, -24(%rbp)
-	jmp	.L17
-.L16:
-	movq	-40(%rbp), %rax
-	movq	%rax, %rdi
-	call	create_new_block
-	movq	%rax, -8(%rbp)
-	movq	-8(%rbp), %rax
-	addq	$16, %rax
-	movq	%rax, -24(%rbp)
-.L17:
-	movq	-24(%rbp), %rax
+mem_alloc_end:
+	addq $40, %rsp								# freeing up stack 40 bytes space
 	movq %rbp, %rsp
 	popq %rbp
 	ret
@@ -185,12 +181,9 @@ memory_alloc:
 memory_free:
 	pushq	%rbp
 	movq	%rsp, %rbp
-	movq	%rdi, -24(%rbp)
-	movq	-24(%rbp), %rax
-	subq	$16, %rax
-	movq	%rax, -8(%rbp)
-	movq	-8(%rbp), %rax
-	movq	$0, (%rax)
-	movl	$0, %eax
+	movq	%rdi, %rax # %rax = %rdi
+	subq	$16, %rax	 # %rax -= 16 -> pointing to the state of the block
+	movq	$0, (%rax) # (%rax) = 0 -> setting block to free
+	movq	$0, %rax	 # %rax = 0
 	popq	%rbp
 	ret
